d19c73afa15874ba2a3b48a0b9fcfb726c15c8a2
judemin <judemin2087@naver.com>
2021-08-17 14:27:54 +0900

    add elements in activity_transmit.xml

--- app/src/main/java/com/eutophia/sound_helper/TransmitActivity.java
+++ app/src/main/java/com/eutophia/sound_helper/TransmitActivity.java
@@ -4,10 +4,29 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import euphony.lib.transmitter.EuTxManager;
+
 public class TransmitActivity extends AppCompatActivity {
+    private String _sendMsg = "";
+
+    EuTxManager mTxManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transmit);
+
+        _sendMsg = "encoded msg from intent";
+
+        mTxManager = new EuTxManager();
+    }
+
+    void transmitMsg(String tmp){
+        mTxManager.euInitTransmit(tmp); // To generate acoustic data
+        mTxManager.process(-1); // generate sound infinite.
+    }
+
+    void stopTransmit(){
+        mTxManager.stop();
     }
 }
