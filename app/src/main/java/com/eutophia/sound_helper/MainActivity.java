79e3df49098bcca8788f2ce2affc0fe5456d7ed5
eunzinn <dhr2176@gmail.com>
2021-08-19 23:43:43 +0900

    update : Update for connection

--- app/src/main/java/com/eutophia/sound_helper/MainActivity.java
+++ app/src/main/java/com/eutophia/sound_helper/MainActivity.java
@@ -2,7 +2,10 @@ package com.eutophia.sound_helper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button activity_inform= (Button)findViewById(R.id.informButton);
+        activity_inform.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), InformActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button activity_listener= (Button)findViewById(R.id.listenerButton);
+        activity_listener.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ListenerActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
