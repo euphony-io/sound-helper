d4fb62672fcd11172d34661e95f4f20ed48381a8
eunzinn <dhr2176@gmail.com>
2021-08-24 23:54:58 +0900

    update : Update for inform and main UI test

--- app/src/androidTest/java/com/eutophia/sound_helper/MainActivityTest.java
+++ app/src/androidTest/java/com/eutophia/sound_helper/MainActivityTest.java
@@ -1,4 +1,29 @@
-import junit.framework.TestCase;
-public class MainActivityTest extends TestCase {
-  
+package com.eutophia.sound_helper;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void testInformButtonClick() {
+        onView(withId(R.id.enter_inform_button)).perform(click());
+    }
+
+    @Test
+    public void testListenerButtonClick() {
+        onView(withId(R.id.enter_listener_button)).perform(click());
+    }
 }
