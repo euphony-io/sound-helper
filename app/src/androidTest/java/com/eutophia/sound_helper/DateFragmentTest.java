11e940f639d1aa7c5a49aaab771e9460559ee612
judemin <judemin2087@naver.com>
2021-08-25 16:13:09 +0900

    add : add DateFragmentTest

---
+++ app/src/androidTest/java/com/eutophia/sound_helper/DateFragmentTest.java
@@ -0,0 +1,12 @@
+package com.eutophia.sound_helper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DateFragmentTest {
+
+    @Test
+    public void onViewCreated() {
+    }
+}
