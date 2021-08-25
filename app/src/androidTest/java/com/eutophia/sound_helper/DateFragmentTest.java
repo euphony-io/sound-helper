4f497c7078dd781ed9ce92746e2ee02fdf18ab9a
judemin <judemin2087@naver.com>
2021-08-25 16:21:30 +0900

    update : updated DateFragmentTest for android unit Test

--- app/src/androidTest/java/com/eutophia/sound_helper/DateFragmentTest.java
+++ app/src/androidTest/java/com/eutophia/sound_helper/DateFragmentTest.java
@@ -1,12 +1,29 @@
 package com.eutophia.sound_helper;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(AndroidJUnit4.class)
 public class DateFragmentTest {
+
+    @Rule
+    public ActivityTestRule activityRule = new ActivityTestRule<>(
+            InfoActivity.class);
 
     @Test
     public void onViewCreated() {
+        onView(withId(R.id.editBirth))
+                .check(matches(isDisplayed()))
+                .perform(click());
     }
 }
